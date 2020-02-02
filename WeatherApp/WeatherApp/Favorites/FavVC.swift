89175e7b317971c4c68af16d41eacc910b3dd945
Tsering Lama <tseringlama@pursuit.org>
2020-02-01 21:38:01 -0500

    set up my tab bar

---
+++ WeatherApp/WeatherApp/Favorites/FavVC.swift
@@ -0,0 +1,16 @@
+//
+//  FavVC.swift
+//  WeatherApp
+//
+//  Created by Tsering Lama on 2/1/20.
+//  Copyright © 2020 David Rifkin. All rights reserved.
+//
+
+import UIKit
+
+class FavVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- WeatherApp/WeatherApp/Tab bar/TabBarController.swift
+++ WeatherApp/WeatherApp/Tab bar/TabBarController.swift
@@ -9,9 +9,22 @@
 import UIKit
 
 class TabBarController: UITabBarController {
+    
+    private lazy var mainVC: MainVC = {
+        let vc = MainVC()
+        vc.tabBarItem = UITabBarItem(title: "Search", image: UIImage(systemName: "magnifyingglass"), tag: 0)
+        return vc
+    }()
+    
+    private lazy var favVC: FavVC = {
+        let vc = FavVC()
+        vc.tabBarItem = UITabBarItem(title: "Favorites", image: UIImage(systemName: "star.fill"), tag: 1)
+        return vc
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        viewControllers = [mainVC,favVC]
     }
     
 
