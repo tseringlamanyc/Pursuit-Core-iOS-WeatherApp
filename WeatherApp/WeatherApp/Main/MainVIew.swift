5b57a7b64a64e348ba4311ffd0cb7c1126ecf886
Tsering Lama <tseringlama@pursuit.org>
2020-02-01 21:29:03 -0500

    setting up scene delegate

---
+++ WeatherApp/WeatherApp/Main/MainVIew.swift
@@ -0,0 +1,26 @@
+//
+//  MainVIew.swift
+//  WeatherApp
+//
+//  Created by Tsering Lama on 2/1/20.
+//  Copyright © 2020 David Rifkin. All rights reserved.
+//
+
+import UIKit
+
+class MainVIew: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: UIScreen.main.bounds)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    private func commonInit() {
+        
+    }
+    
+    
+}
---
+++ WeatherApp/WeatherApp/Tab bar/TabBarController.swift
@@ -0,0 +1,18 @@
+//
+//  TabBarController.swift
+//  WeatherApp
+//
+//  Created by Tsering Lama on 2/1/20.
+//  Copyright © 2020 David Rifkin. All rights reserved.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+}
